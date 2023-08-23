1c1c4e4e32858960820a8c88154c07068f1bfdb4
Sainath Landge <sainath.landge22@gmail.com>
2023-08-23 13:09:07 +0530

    Add project files.

---
+++ Controllers/QuestionerController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionerController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public QuestionerController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public IActionResult GetQuestions()
+        {
+            try
+            {
+                string filePath = _configuration.GetSection("JsonDataSource").Value;
+                using(StreamReader reader = new StreamReader(filePath))
+                {
+                    string question = reader.ReadToEnd();
+                    return Ok(question);
+                }
+
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500,"Something went wrong");
+            }
+        }
+    }
+}
